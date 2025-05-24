aa87a2f4689b3108222f92204aedbcc626c01cab
Pendar <pendar.ra@gmail.com>
2025-05-24 11:37:23 +0330

    added signup and signin

--- src/main/java/org/foodapp/Main.java
+++ src/main/java/org/foodapp/Main.java
@@ -1,9 +1,17 @@
 package org.foodapp;
 
 
+import Controller.UserHttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello World");
-        System.out.print("second try");
+    public static void main(String[] args) throws Exception{
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080),0);
+        server.createContext("/user", new UserHttpHandler());
+
+        server.start();
+        System.out.println("server started");
     }
 }
