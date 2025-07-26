a3d555868185c11d9e575d87ad1972b91c7e8c0c
DANIALSEYEDI <danielseyedi84@gmail.com>
2025-07-26 12:15:48 +0330

    edit transaction response

--- src/main/java/org/foodapp/dto/Response/TransactionsResponse.java
+++ src/main/java/org/foodapp/dto/Response/TransactionsResponse.java
@@ -1,12 +1,14 @@
 package org.foodapp.dto.Response;
-
 import org.foodapp.model.Transaction;
+import java.math.BigDecimal;
+
 public class TransactionsResponse {
     public Long id;
     public Long order_id;
     public Long user_id;
     public String method;
     public String status;
+    public BigDecimal amount;
 
     public static TransactionsResponse from(Transaction tx) {
         TransactionsResponse dto = new TransactionsResponse();
@@ -15,7 +17,7 @@ public class TransactionsResponse {
         dto.user_id = tx.getUser() != null ? tx.getUser().getId() : null;
         dto.method = tx.getMethod().name().toLowerCase();
         dto.status = tx.getStatus().name().toLowerCase();
+        dto.amount=tx.getAmount();
         return dto;
     }
-
 }
